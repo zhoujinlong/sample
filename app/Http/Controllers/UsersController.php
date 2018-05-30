4b5c5ddcc4239a5267ead7f46f3a3e410668252f
周金龙 <280227603@qq.com>
2018-05-30 16:43:04 +0000

    Finish edit

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -7,6 +7,17 @@ use App\Models\User;
 
 class UsersController extends Controller
 {
+	public function __construct()
+	{
+		$this->middleware('auth', [            
+            'except' => ['show', 'create', 'store']
+        ]);
+
+        $this->middleware('guest', [
+            'only' => ['create']
+        ]);
+	}
+
     public function create(){
 
     	return view('users.create');
@@ -36,5 +47,36 @@ class UsersController extends Controller
         session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
 
         return redirect()->route('users.show', [$user]);
+    }
+
+    public function edit(User $user)
+    {
+    	$this->authorize('update', $user);
+    	return view('users.edit', compact('user'));
+
+    }
+
+    public function update(User $user, Request $request)
+    {
+    	$this->validate($request, [
+            'name' => 'required|max:50',
+            'password' => 'nullable|confirmed|min:6'
+        ]);
+
+        $this->authorize('update', $user);
+
+    	$data = [];
+    	$data['name'] = $request->name;
+    	if ($request->password)
+    	{
+    		$data['password'] = bcrypt($request->password);
+    	}
+
+        $user->update($data);
+
+        session()->flash('success', '个人资料更新成功！');
+
+        return redirect()->route('users.show', $user->id);
+
     }
 }
