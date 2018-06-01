68d17e289c1cb1f1da5e2ac843326ca0281a2b89
周金龙 <280227603@qq.com>
2018-06-01 15:00:29 +0000

    Finish user edit,update,index,and destroy actions

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -10,13 +10,20 @@ class UsersController extends Controller
 	public function __construct()
 	{
 		$this->middleware('auth', [            
-            'except' => ['show', 'create', 'store']
+            'except' => ['show', 'create', 'store', 'index']
         ]);
 
         $this->middleware('guest', [
             'only' => ['create']
         ]);
 	}
+
+    public function index(){
+        $users = User::paginate(10);
+
+        return view('users.index', compact('users'));
+
+    }
 
     public function create(){
 
@@ -77,6 +84,18 @@ class UsersController extends Controller
         session()->flash('success', '个人资料更新成功！');
 
         return redirect()->route('users.show', $user->id);
+
+    }
+
+    public function destroy(User $user){
+
+        $this->authorize('destroy', $user);
+
+        $user->delete();
+
+        session()->flash('success', '用户删除成功');
+
+        return redirect()->back();
 
     }
 }
